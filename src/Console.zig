699dcea1e06248cc60d27177bffbb290308c6372
Chloé Vulquin <toast@bunkerlabs.net>
2026-08-21 07:48:35 +0200

    Console: zig fmt

--- src/Console.zig
+++ src/Console.zig
@@ -1,10 +1,10 @@
 //! Wraps UEFI's con_in and con_out to handle input and output.
-const std  = @import("std");
+const std = @import("std");
 const uefi = std.os.uefi;
-const uni  = std.unicode;
-const Writer       = std.Io.Writer;
-const STI          = uefi.protocol.SimpleTextInput;
-const STO          = uefi.protocol.SimpleTextOutput;
+const uni = std.unicode;
+const Writer = std.Io.Writer;
+const STI = uefi.protocol.SimpleTextInput;
+const STO = uefi.protocol.SimpleTextOutput;
 const BootServices = uefi.tables.BootServices;
 
 const text = @import("text.zig");
@@ -15,7 +15,7 @@ const Console = @This();
 writer: Writer,
 
 /// The backing SimpleTextInput instance.
-input:  *STI,
+input: *STI,
 
 /// The backing SimpleTextOutput instance.
 output: *STO,
@@ -41,19 +41,20 @@ pub fn getCursor(self: *Console) union(enum) {
     return .{ .enabled = .{
         .row = @intCast(mode.cursor_row),
         .col = @intCast(mode.cursor_column),
-    }};
+    } };
 }
 
 /// Sets the cursor's position or status.
 pub fn setCursor(
     self: *Console,
     state: union(enum) {
-        enabled, disabled,
+        enabled,
+        disabled,
         position: Position,
     },
 ) !void {
     switch (state) {
-        .enabled  => try self.output.enableCursor(true),
+        .enabled => try self.output.enableCursor(true),
         .disabled => try self.output.enableCursor(false),
         .position => |p| {
             try self.setCursor(.enabled);
@@ -75,25 +76,20 @@ fn drainStr(out: *STO, str: []const u8) Writer.Error!usize {
     while (it.nextCodepoint()) |char| {
         if (char > 0xffff) return error.WriteFailed;
 
-        _ = out.outputString(&[_:0]u16{@truncate(char)})
-            catch { return error.WriteFailed; }
-            or    return error.WriteFailed;
-        size += uni.utf8CodepointSequenceLength(char)
-            catch return error.WriteFailed;
+        _ = out.outputString(&[_:0]u16{@truncate(char)}) catch {
+            return error.WriteFailed;
+        } or return error.WriteFailed;
+        size += uni.utf8CodepointSequenceLength(char) catch return error.WriteFailed;
     }
     return size;
 }
 
 /// An implementation of std.Io.Writer.VTable.drain.
-fn drain (
-    w: *Writer,
-    data: []const []const u8,
-    splat: usize
-) Writer.Error!usize {
+fn drain(w: *Writer, data: []const []const u8, splat: usize) Writer.Error!usize {
     const self: *Console = @fieldParentPtr("writer", w);
 
     // simplify later splatting logic
-    const strs = if (splat == 0) data[0..data.len - 1] else data;
+    const strs = if (splat == 0) data[0 .. data.len - 1] else data;
 
     var out: usize = 0;
     // w.end is never >0 if the implementation is unbuffered
@@ -132,54 +128,54 @@ pub const Input = union(enum(u16)) {
     text: u21 = 0x0,
 
     // SIMPLE_TEXT_INPUT
-    up_arrow    = 0x1,
-    down_arrow  = 0x2,
+    up_arrow = 0x1,
+    down_arrow = 0x2,
     right_arrow = 0x3,
-    left_arrow  = 0x4,
-    home        = 0x5,
-    end         = 0x6,
-    insert      = 0x7,
-    delete      = 0x8,
-    page_up     = 0x9,
-    page_down   = 0xa,
-    fn1         = 0xb,
-    fn2         = 0xc,
-    fn3         = 0xd,
-    fn4         = 0xe,
-    fn5         = 0xf,
-    fn6         = 0x10,
-    fn7         = 0x11,
-    fn8         = 0x12,
-    fn9         = 0x13,
-    fn10        = 0x14,
-    escape      = 0x17,
+    left_arrow = 0x4,
+    home = 0x5,
+    end = 0x6,
+    insert = 0x7,
+    delete = 0x8,
+    page_up = 0x9,
+    page_down = 0xa,
+    fn1 = 0xb,
+    fn2 = 0xc,
+    fn3 = 0xd,
+    fn4 = 0xe,
+    fn5 = 0xf,
+    fn6 = 0x10,
+    fn7 = 0x11,
+    fn8 = 0x12,
+    fn9 = 0x13,
+    fn10 = 0x14,
+    escape = 0x17,
 
     // SIMPLE_TEXT_INPUT_EX
-    fn11            = 0x15,
-    fn12            = 0x16,
-    pause           = 0x48,
-    fn13            = 0x68,
-    fn14            = 0x69,
-    fn15            = 0x6a,
-    fn16            = 0x6b,
-    fn17            = 0x6c,
-    fn18            = 0x6d,
-    fn19            = 0x6e,
-    fn20            = 0x6f,
-    fn21            = 0x70,
-    fn22            = 0x71,
-    fn23            = 0x72,
-    fn24            = 0x73,
-    mute            = 0x7f,
-    vol_up          = 0x80,
-    vol_down        = 0x81,
-    brightness_up   = 0x100,
+    fn11 = 0x15,
+    fn12 = 0x16,
+    pause = 0x48,
+    fn13 = 0x68,
+    fn14 = 0x69,
+    fn15 = 0x6a,
+    fn16 = 0x6b,
+    fn17 = 0x6c,
+    fn18 = 0x6d,
+    fn19 = 0x6e,
+    fn20 = 0x6f,
+    fn21 = 0x70,
+    fn22 = 0x71,
+    fn23 = 0x72,
+    fn24 = 0x73,
+    mute = 0x7f,
+    vol_up = 0x80,
+    vol_down = 0x81,
+    brightness_up = 0x100,
     brightness_down = 0x101,
-    @"suspend"      = 0x102,
-    hibernate       = 0x103,
-    toggle_display  = 0x104,
-    recovery        = 0x105,
-    eject           = 0x106,
+    @"suspend" = 0x102,
+    hibernate = 0x103,
+    toggle_display = 0x104,
+    recovery = 0x105,
+    eject = 0x106,
     // 0x8000-0xffff oem reserved
 
     /// Translates from a SimpleTextInput tuple to our logical format.
@@ -187,7 +183,7 @@ pub const Input = union(enum(u16)) {
         return switch (input.scan_code) {
             0 => .{ .text = input.unicode_char },
             inline 0x1...0x14, 0x17 => |sc| blk: {
-                const inputTag: std.meta.Tag(Input) = @enumFromInt(sc);
+                const inputTag: std.meta.Tag(Input) = @fromBackingInt(@intCast(sc));
                 break :blk @unionInit(Input, @tagName(inputTag), {});
             },
             else => unreachable, // at least according to the spec :^)
@@ -198,16 +194,16 @@ pub const Input = union(enum(u16)) {
     pub fn toInput(self: Input) STI.Key.Input {
         return switch (self) {
             .text => |c| .{ .scan_code = 0, .unicode_char = @truncate(c) },
-            else => .{ .scan_code = @intFromEnum(self) }
+            else => .{ .scan_code = @backingInt(self) },
         };
     }
 };
 
 /// Convenience function to wait for either a keypress or an event to fire.
 /// Returns which event fired.
-pub fn waitForKey(self: *Console, event: ?uefi.Event) !enum{ input, event } {
+pub fn waitForKey(self: *Console, event: ?uefi.Event) !enum { input, event } {
     const bs = uefi.system_table.boot_services.?;
-    var events = [2]uefi.Event{self.input.wait_for_key, self.input.wait_for_key};
+    var events = [2]uefi.Event{ self.input.wait_for_key, self.input.wait_for_key };
     if (event) |e| {
         events[1] = e;
         _, const idx = try bs.waitForEvent(events[0..]);
@@ -229,11 +225,11 @@ pub inline fn readInput(self: *Console) !Input {
 /// it's safe to create multiple instances.
 pub fn init() Console {
     return .{
-        .writer= Writer {
+        .writer = Writer{
             .buffer = &.{},
             .vtable = &VTable,
         },
-        .input  = uefi.system_table.con_in.?,
+        .input = uefi.system_table.con_in.?,
         .output = uefi.system_table.con_out.?,
     };
 }
